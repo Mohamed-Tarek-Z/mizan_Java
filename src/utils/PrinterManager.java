5e38aa7425a178a95dca825e5e8df95b6f8699f2
Mohamed-Tarek-Z <MohamedTarek153@gmail.com>
2026-09-17 11:11:59 +0300

    fix: auto fit paper in printer manger

--- src/utils/PrinterManager.java
+++ src/utils/PrinterManager.java
@@ -1,19 +1,15 @@
 package utils;
 
 import exceptions.BusinessException;
+import java.awt.Graphics;
 import java.awt.Graphics2D;
-import java.awt.RenderingHints;
-import java.awt.image.BufferedImage;
+import java.awt.print.PageFormat;
+import java.awt.print.Paper;
 import java.awt.print.Printable;
 import java.awt.print.PrinterException;
 import java.awt.print.PrinterJob;
 import javax.print.*;
-import javax.print.attribute.HashPrintRequestAttributeSet;
-import javax.print.attribute.PrintRequestAttributeSet;
 import java.util.Arrays;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import javax.print.attribute.standard.MediaPrintableArea;
 import javax.swing.JPanel;
 import javax.swing.SwingUtilities;
 
@@ -21,10 +17,12 @@ public class PrinterManager {
 
     private final ErrorListener errorListener;
     private PrintService TicketPrinter;
+    private final int printerDPI = 203;
 
     public PrinterManager(ErrorListener errorListener) throws BusinessException {
         this.errorListener = errorListener;
-        this.TicketPrinter = getPrinterByName(utils.CheckConfigFileAndFolder().getProperty("ticketPrinterName", "Microsoft Print"));
+        this.TicketPrinter = getPrinterByName(
+                utils.CheckConfigFileAndFolder().getProperty("ticketPrinterName", "Microsoft Print"));
     }
 
     /**
@@ -33,7 +31,8 @@ public class PrinterManager {
      * @throws exceptions.BusinessException
      */
     public void reInitPrinters() throws BusinessException {
-        this.TicketPrinter = getPrinterByName(utils.CheckConfigFileAndFolder().getProperty("ticketPrinterName", "Microsoft Print"));
+        this.TicketPrinter = getPrinterByName(
+                utils.CheckConfigFileAndFolder().getProperty("ticketPrinterName", "Microsoft Print"));
     }
 
     /**
@@ -58,50 +57,61 @@ public class PrinterManager {
      */
     public void printPanelToImage(JPanel panel) {
         SwingUtilities.invokeLater(() -> {
-            int printerDPI = 203;
-            int width = (int) (panel.getWidth() * printerDPI / 72.0);
-            int height = (int) (panel.getHeight() * printerDPI / 72.0);
-            double scaleX = width / (double) panel.getWidth();
-            double scaleY = height / (double) panel.getHeight();
-            double scale = Math.min(scaleX, scaleY);
-
-            BufferedImage panelImage = new BufferedImage(width, height, BufferedImage.TYPE_BYTE_GRAY);
-            Graphics2D g2d = panelImage.createGraphics();
-            g2d.scale(scale, scale);
-            panel.printAll(g2d);
-            g2d.dispose();
-            //ImageIO.write(panelImage, "PNG", new File(System.getProperty("user.dir") + "\\Temp\\debug_print.png"));
-            new Thread(() -> {
-                try {
-                    printImage(panelImage);
-                } catch (BusinessException ex) {
-                    Logger.getLogger(PrinterManager.class.getName()).log(Level.SEVERE, null, ex);
-                    errorListener.onError(ex);
+            try {
+                panel.setSize(300, 350);
+                if (TicketPrinter == null) {
+                    throw new BusinessException(
+                            utils.CheckConfigFileAndFolder().getProperty("ticketPrinterName", "Microsoft Print")
+                            + "Ticket printer not found!");
                 }
-            }).start();
+                PrinterJob printerJob = PrinterJob.getPrinterJob();
+                printerJob.setPrintService(TicketPrinter);
+                Paper paper = new Paper();
+                PageFormat pageFormat = printerJob.defaultPage();
+
+                paper.setSize(pageFormat.getWidth(), pageFormat.getHeight());
+                paper.setImageableArea(0, 0, pageFormat.getWidth(), pageFormat.getHeight());
+
+                pageFormat.setPaper(paper);
+                pageFormat.setOrientation(PageFormat.PORTRAIT);
+
+                // Let the printer adjust the PageFormat if necessary
+                pageFormat = printerJob.validatePage(pageFormat);
+
+                printerJob.setPrintable(new Printable() {
+                    @Override
+                    public int print(Graphics graphics, PageFormat pf, int pageIndex) throws PrinterException {
+                        if (pageIndex > 0) {
+                            return NO_SUCH_PAGE;
+                        }
+                        Graphics2D g2 = (Graphics2D) graphics.create();
+                        try {
+
+                            double scaleX = pf.getImageableWidth() / panel.getWidth();
+                            double scaleY = pf.getImageableHeight() / panel.getHeight();
+
+                            g2.translate(pf.getImageableX()+5, pf.getImageableY());
+                            g2.scale(scaleX, scaleY);
+                            panel.printAll(g2);
+
+                        } finally {
+                            g2.dispose();
+                        }
+                        return PAGE_EXISTS;
+                    }
+                }, pageFormat);
+                new Thread(() -> {
+                    try {
+                        printerJob.print();
+                    } catch (PrinterException ex) {
+                        errorListener.onError(ex);
+                    }
+                }).start();
+
+            } catch (PrinterException ex) {
+                errorListener.onError(ex);
+            }
         });
     }
 
-    private void printImage(BufferedImage image) throws BusinessException {
-        try {
-            if (TicketPrinter == null) {
-                throw new BusinessException(utils.CheckConfigFileAndFolder().getProperty("ticketPrinterName", "Microsoft Print") + "Ticket printer not found!");
-            }
-            PrinterJob job = PrinterJob.getPrinterJob();
-            job.setPrintService(TicketPrinter);
-            job.setPrintable((graphics, pageFormat, pageIndex) -> {
-                if (pageIndex > 0) {
-                    return Printable.NO_SUCH_PAGE;
-                }
-                Graphics2D g2d = (Graphics2D) graphics;
-                g2d.drawImage(image, 0, 0, 760, 486, null); // these number after many testing with 10 x 10 paper Size
-                return Printable.PAGE_EXISTS;
-            });
-            PrintRequestAttributeSet attributes = new HashPrintRequestAttributeSet();
-            attributes.add(new MediaPrintableArea(0, 0, 100, 100, MediaPrintableArea.MM));
-            job.print(attributes);
-        } catch (PrinterException e) {
-            throw new BusinessException("error in panel Printing");
-        }
-    }
 }
